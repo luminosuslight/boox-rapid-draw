f55b0bd20286a1c57c5127fc2c1f0e473ae071d2
Tim Henning <tim@luminosuslight.de>
2026-06-07 11:24:31 +0200

    Add signing configuration for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,19 @@ android {
             abiFilters += setOf("armeabi-v7a", "arm64-v8a")
         }
     }
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_PATH"))
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
 
     buildTypes {
         release {
             isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
